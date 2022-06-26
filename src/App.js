d72813d5039c7fb37e23a0064301b6180fbede33
Larissa Perinoto <perinotolarissa@gmail.com>
2022-06-26 19:41:17 -0300

    [Project Tryunfo] - Requisito 8.

--- src/App.js
+++ src/App.js
@@ -100,6 +100,8 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled } = this.state;
+
+    const { data } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
@@ -128,6 +130,23 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        <ul>
+          { data.map((card, index) => (
+            <div key={ index }>
+              <Card
+                cardName={ card.cardName }
+                cardDescription={ card.cardDescription }
+                cardAttr1={ card.cardAttr1 }
+                cardAttr2={ card.cardAttr2 }
+                cardAttr3={ card.cardAttr3 }
+                cardImage={ card.cardImage }
+                cardRare={ card.cardRare }
+                cardTrunfo={ card.cardTrunfo }
+              />
+            </div>
+          ))}
+        </ul>
+
       </div>
     );
   }
